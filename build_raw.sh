07b4f704c939590bfa9aac26484ecedf8b33f5b7
FranklineMisango <franklinemisango@gmail.com>
2025-07-09 19:17:28 +0200

    feat: Remove obsolete ISO and raw build scripts

--- build_raw.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Create a raw binary version of the kernel
-x86_64-elf-objcopy -O binary build/tradekernel_kernel build/tradekernel.bin
-
-echo "Created raw binary: build/tradekernel.bin"
