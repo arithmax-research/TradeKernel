a0787542b6f5ac8b5833e64603e4d83fa4f5221e
FranklineMisango <franklinemisango@gmail.com>
2025-10-03 06:03:58 +0800

    feat: Update hello.asm to use Linux syscall for output and exit

--- LC3_baseline/MacTutorials/tools/hello.asm
+++ LC3_baseline/MacTutorials/tools/hello.asm
@@ -1,16 +1,16 @@
 section .data
-	hello:     db 'Hello, World!',10    ; 'Hello, World!' plus a linefeed character
-	helloLen:  equ $-hello             ; Length of the 'Hello world!' string
+    hello:     db 'Hello, World!',10
+    helloLen:  equ $-hello
 
 section .text
-	global _main
+    global _start
 
-_main:
-	mov rax, 0x2000004    ; macOS write syscall
-	mov rdi, 1            ; File descriptor 1 - standard output
-	lea rsi, [rel hello]  ; Load effective address of hello
-	mov rdx, helloLen     ; helloLen is a constant
-	syscall               ; Call the kernel
-	mov rax, 0x2000001    ; macOS exit syscall
-	mov rdi, 0            ; Exit with return "code" of 0 (no error)
-	syscall
+_start:
+    mov eax,4            ; sys_write
+    mov ebx,1            ; stdout
+    mov ecx,hello
+    mov edx,helloLen
+    int 80h              ; Linux syscall
+    mov eax,1            ; sys_exit
+    mov ebx,0
+    int 80h
